59fec0ea955106cba3ee5ea8bbb7bf86b3285a24
ShyamalaGowri <ShyamalaGowri@users.noreply.github.com>
2021-08-05 21:54:55 +0530

    feat: added support for v3 api's
    * Integration tests added
    
    * feat(v3 api): adding v3 api test cases.
    
    * feat(v3 api): adding v3 api test cases.
    
    * updated git hub password
    
    * removed token from travis.yml
    
    Co-authored-by: shyamala <shyamala@macbook-pro>
    Co-authored-by: shyamalagowri <shyamalagowri@192.168.1.7>

---
+++ ibmanalyticsengineapiv3/ibm_analytics_engine_api_v3_examples_test.go
@@ -0,0 +1,231 @@
+// +build examples
+
+/**
+ * (C) Copyright IBM Corp. 2021.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ibmanalyticsengineapiv3_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/IBM/go-sdk-core/v5/core"
+	"github.com/IBM/ibm-iae-go-sdk/ibmanalyticsengineapiv3"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"os"
+)
+
+//
+// This file provides an example of how to use the IBM Analytics Engine API service.
+//
+// The following configuration properties are assumed to be defined:
+// IBM_ANALYTICS_ENGINE_API_URL=<service base url>
+// IBM_ANALYTICS_ENGINE_API_AUTH_TYPE=iam
+// IBM_ANALYTICS_ENGINE_API_APIKEY=<IAM apikey>
+// IBM_ANALYTICS_ENGINE_API_AUTH_URL=<IAM token service base URL - omit this if using the production environment>
+//
+// These configuration properties can be exported as environment variables, or stored
+// in a configuration file and then:
+// export IBM_CREDENTIALS_FILE=<name of configuration file>
+//
+const externalConfigFile = "../ibm_analytics_engine_api_v3.env"
+
+var (
+	ibmAnalyticsEngineApiService *ibmanalyticsengineapiv3.IbmAnalyticsEngineApiV3
+	config       map[string]string
+	configLoaded bool = false
+)
+
+func shouldSkipTest() {
+	if !configLoaded { 
+		Skip("External configuration is not available, skipping tests...")
+	}
+}
+
+var _ = Describe(`IbmAnalyticsEngineApiV3 Examples Tests`, func() {
+	Describe(`External configuration`, func() {
+		It("Successfully load the configuration", func() {
+			var err error
+			_, err = os.Stat(externalConfigFile)
+			if err != nil {
+				Skip("External configuration file not found, skipping tests: " + err.Error())
+			}
+
+			os.Setenv("IBM_CREDENTIALS_FILE", externalConfigFile)
+			config, err = core.GetServiceProperties(ibmanalyticsengineapiv3.DefaultServiceName)
+			if err != nil {
+				Skip("Error loading service properties, skipping tests: " + err.Error())
+			}
+
+			configLoaded = len(config) > 0
+		})
+	})
+
+	Describe(`Client initialization`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It("Successfully construct the service client instance", func() {
+			var err error
+
+			// begin-common
+
+			ibmAnalyticsEngineApiServiceOptions := &ibmanalyticsengineapiv3.IbmAnalyticsEngineApiV3Options{}
+
+			ibmAnalyticsEngineApiService, err = ibmanalyticsengineapiv3.NewIbmAnalyticsEngineApiV3UsingExternalConfig(ibmAnalyticsEngineApiServiceOptions)
+
+			if err != nil {
+				panic(err)
+			}
+
+			// end-common
+
+			Expect(ibmAnalyticsEngineApiService).ToNot(BeNil())
+		})
+	})
+
+	Describe(`IbmAnalyticsEngineApiV3 request examples`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`GetInstanceByID request example`, func() {
+			// begin-get_instance_by_id
+
+			getInstanceByIdOptions := ibmAnalyticsEngineApiService.NewGetInstanceByIdOptions(
+				"testString",
+			)
+
+			instanceDetails, response, err := ibmAnalyticsEngineApiService.GetInstanceByID(getInstanceByIdOptions)
+			if err != nil {
+				panic(err)
+			}
+			b, _ := json.MarshalIndent(instanceDetails, "", "  ")
+			fmt.Println(string(b))
+
+			// end-get_instance_by_id
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(instanceDetails).ToNot(BeNil())
+
+		})
+		It(`CreateApplication request example`, func() {
+			// begin-create_application
+
+			createApplicationOptions := ibmAnalyticsEngineApiService.NewCreateApplicationOptions(
+				"testString",
+			)
+
+			applicationResponse, response, err := ibmAnalyticsEngineApiService.CreateApplication(createApplicationOptions)
+			if err != nil {
+				panic(err)
+			}
+			b, _ := json.MarshalIndent(applicationResponse, "", "  ")
+			fmt.Println(string(b))
+
+			// end-create_application
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(201))
+			Expect(applicationResponse).ToNot(BeNil())
+
+		})
+		It(`GetApplications request example`, func() {
+			// begin-get_applications
+
+			getApplicationsOptions := ibmAnalyticsEngineApiService.NewGetApplicationsOptions(
+				"testString",
+			)
+
+			applicationCollection, response, err := ibmAnalyticsEngineApiService.GetApplications(getApplicationsOptions)
+			if err != nil {
+				panic(err)
+			}
+			b, _ := json.MarshalIndent(applicationCollection, "", "  ")
+			fmt.Println(string(b))
+
+			// end-get_applications
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(applicationCollection).ToNot(BeNil())
+
+		})
+		It(`GetApplicationByID request example`, func() {
+			// begin-get_application_by_id
+
+			getApplicationByIdOptions := ibmAnalyticsEngineApiService.NewGetApplicationByIdOptions(
+				"testString",
+				"testString",
+			)
+
+			applicationGetResponse, response, err := ibmAnalyticsEngineApiService.GetApplicationByID(getApplicationByIdOptions)
+			if err != nil {
+				panic(err)
+			}
+			b, _ := json.MarshalIndent(applicationGetResponse, "", "  ")
+			fmt.Println(string(b))
+
+			// end-get_application_by_id
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(applicationGetResponse).ToNot(BeNil())
+
+		})
+		It(`GetApplicationState request example`, func() {
+			// begin-get_application_state
+
+			getApplicationStateOptions := ibmAnalyticsEngineApiService.NewGetApplicationStateOptions(
+				"testString",
+				"testString",
+			)
+
+			applicationGetStateResponse, response, err := ibmAnalyticsEngineApiService.GetApplicationState(getApplicationStateOptions)
+			if err != nil {
+				panic(err)
+			}
+			b, _ := json.MarshalIndent(applicationGetStateResponse, "", "  ")
+			fmt.Println(string(b))
+
+			// end-get_application_state
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(applicationGetStateResponse).ToNot(BeNil())
+
+		})
+		It(`DeleteApplicationByID request example`, func() {
+			// begin-delete_application_by_id
+
+			deleteApplicationByIdOptions := ibmAnalyticsEngineApiService.NewDeleteApplicationByIdOptions(
+				"testString",
+				"testString",
+			)
+
+			response, err := ibmAnalyticsEngineApiService.DeleteApplicationByID(deleteApplicationByIdOptions)
+			if err != nil {
+				panic(err)
+			}
+
+			// end-delete_application_by_id
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(204))
+
+		})
+	})
+})
--- ibmanalyticsengineapiv3/ibm_analytics_engine_api_v3_integration_test.go
+++ ibmanalyticsengineapiv3/ibm_analytics_engine_api_v3_integration_test.go
@@ -1,106 +1,222 @@
 // +build integration
 
+/**
+ * (C) Copyright IBM Corp. 2021.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ibmanalyticsengineapiv3_test
 
 import (
-    "github.com/joho/godotenv"
-    . "github.com/onsi/ginkgo"
-    . "github.com/onsi/gomega"
-    "os"
-   	"github.com/IBM/go-sdk-core/v3/core"
+	"fmt"
+	"github.com/IBM/go-sdk-core/v5/core"
 	"github.com/IBM/ibm-iae-go-sdk/ibmanalyticsengineapiv3"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"os"
 )
 
 /**
- * This class contains an integration test for the IBM Analytics Engine service.
+ * This file contains an integration test for the ibmanalyticsengineapiv3 package.
  *
  * Notes:
  *
- * 1. This IBM Analytics Engine integration test shows how to automatically skip tests if the required config file
- *    is not available.
- *
- * 2. Before running this test:
- *    a. "cp example-service.env.hide ibmanalyticsengine-service.env"
- *    b. start up the IbmAnalyticsEngineApi service by following the instructions here:
- *    https://github.com/IBM/ibm-iae-go-sdk/blob/master/HOW_TO_TEST.md
+ * The integration test will automatically skip tests if the required config file is not available.
  */
-const externalConfigFile = "../ibmanalyticsengine-service.env"
 
-var (
-    service      *ibmanalyticsengineapiv3.IbmAnalyticsEngineApiV3
-    err          error
-    configLoaded bool = false
-    instanceGuid string
-)
+var _ = Describe(`IbmAnalyticsEngineApiV3 Integration Tests`, func() {
 
-func shouldSkipTest() {
-    if !configLoaded {
-        Skip("External configuration is not available, skipping...")
-    }
-}
+	const externalConfigFile = "../ibmanalyticsengine-service.env"
 
-var _ = Describe(`IbmAnalyticsEngineApiV3`, func() {
-    It("Successfully load the configuration", func() {
-        err = godotenv.Load(externalConfigFile)
-        if err == nil {
-            //
-            // Retrieve any test-specific properties from the environment.
-            instanceGuid = os.Getenv("IBM_ANALYTICS_ENGINE_INSTANCE_GUID")
+	var (
+		err          error
+		ibmAnalyticsEngineApiService *ibmanalyticsengineapiv3.IbmAnalyticsEngineApiV3
+		serviceURL   string
+		config       map[string]string
+		// Retrieve any test-specific properties from the environment.
+        instanceGuid string
+        applicationId string
+		
+	)
 
-            // Set the flag to allow tests to execute.
-            configLoaded = true
-        }
+	var shouldSkipTest = func() {
+		Skip("External configuration is not available, skipping tests...")
+	}
 
-        if !configLoaded {
-            Skip("External configuration could not be loaded, skipping...")
-        }
-    })
+	Describe(`External configuration`, func() {
+		It("Successfully load the configuration", func() {
+			_, err = os.Stat(externalConfigFile)
+			if err != nil {
+				Skip("External configuration file not found, skipping tests: " + err.Error())
+			}
 
-    It("Successfully create the service client instance", func() {
-        shouldSkipTest()
+			os.Setenv("IBM_CREDENTIALS_FILE", externalConfigFile)
+			config, err = core.GetServiceProperties(ibmanalyticsengineapiv3.DefaultServiceName)
+			if err != nil {
+				Skip("Error loading service properties, skipping tests: " + err.Error())
+			}
+			serviceURL = config["URL"]
+			if serviceURL == "" {
+				Skip("Unable to load service URL configuration property, skipping tests")
+			}
+			instanceGuid = os.Getenv("IBM_ANALYTICS_ENGINE_INSTANCE_GUID")
+			fmt.Printf("Service URL: %s\n", serviceURL)
+			fmt.Printf("instanceGuid: %s\n", instanceGuid)
+			shouldSkipTest = func() {}
+		})
+	})
 
-        service, err = ibmanalyticsengineapiv3.NewIbmAnalyticsEngineApiV3UsingExternalConfig(
-            &ibmanalyticsengineapiv3.IbmAnalyticsEngineApiV3Options{})
-        Expect(err).To(BeNil())
-        Expect(service).ToNot(BeNil())
-        Expect(service.Service.Options.URL).To(Not(Equal("")))
-    })
-
-   //  Describe(`GetAnalyticsEngineByID(getAnalyticsEngineByIdOptions *GetAnalyticsEngineByIdOptions)`, func() {
-   //      It(`Invoke GetAnalyticsEngineByID successfully`, func() {
-   //          shouldSkipTest()
-
-			// // Construct an instance of the GetAnalyticsEngineByIdOptions model
-			// getAnalyticsEngineByIdOptionsModel := new(ibmanalyticsengineapiv2.GetAnalyticsEngineByIdOptions)
-			// getAnalyticsEngineByIdOptionsModel.InstanceGuid = core.StringPtr(instanceGuid)
-
-   //          result, response, err := service.GetAnalyticsEngineByID(getAnalyticsEngineByIdOptionsModel)
-   //          Expect(err).To(BeNil())
-   //          Expect(response).ToNot(BeNil())
-   //          Expect(response.StatusCode).To(Equal(200))
-   //          Expect(result).ToNot(BeNil())
-
-   //          // resources := result.Resources
-   //      })
-   //  })
-
-	Describe(`GetInstanceByID(getInstanceByIdOptions *GetInstanceByIdOptions)`, func() {
-		It(`Invoke GetInstanceByID successfully`, func() {
+	Describe(`Client initialization`, func() {
+		BeforeEach(func() {
 			shouldSkipTest()
+		})
+		It("Successfully construct the service client instance", func() {
 
-			// Construct an instance of the GetInstanceByIdOptions model
-			getInstanceByIdOptionsModel := new(ibmanalyticsengineapiv3.GetInstanceByIdOptions)
-			getInstanceByIdOptionsModel.InstanceID = core.StringPtr(instanceGuid)
-			getInstanceByIdOptionsModel.Headers = map[string]string{"x-custom-header": "x-custom-value"}
+			ibmAnalyticsEngineApiServiceOptions := &ibmanalyticsengineapiv3.IbmAnalyticsEngineApiV3Options{}
 
-			// Expect response parsing to fail since we are receiving a text/plain response
-			result, response, err := service.GetInstanceByID(getInstanceByIdOptionsModel)
-            Expect(err).To(BeNil())
-            Expect(response).ToNot(BeNil())
-            Expect(response.StatusCode).To(Equal(200))
-            Expect(result).ToNot(BeNil())
+			ibmAnalyticsEngineApiService, err = ibmanalyticsengineapiv3.NewIbmAnalyticsEngineApiV3UsingExternalConfig(ibmAnalyticsEngineApiServiceOptions)
+
+			Expect(err).To(BeNil())
+			Expect(ibmAnalyticsEngineApiService).ToNot(BeNil())
+			Expect(ibmAnalyticsEngineApiService.Service.Options.URL).To(Equal(serviceURL))
+		})
+	})
+
+	Describe(`GetInstanceByID - Find instance by id`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`GetInstanceByID(getInstanceByIdOptions *GetInstanceByIdOptions)`, func() {
+			fmt.Fprint(os.Stdout, fmt.Sprintf("instance id  value is - %v", instanceGuid))
+			
+			getInstanceByIdOptions := &ibmanalyticsengineapiv3.GetInstanceByIdOptions{
+				InstanceID: core.StringPtr(instanceGuid),
+			}
+
+			instanceDetails, response, err := ibmAnalyticsEngineApiService.GetInstanceByID(getInstanceByIdOptions)
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(instanceDetails).ToNot(BeNil())
 
 		})
 	})
 
+	Describe(`CreateApplication - Deploys a Spark application in the Analytics Engine instance`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`CreateApplication(createApplicationOptions *CreateApplicationOptions)`, func() {
+
+			createApplicationOptions := &ibmanalyticsengineapiv3.CreateApplicationOptions{
+				InstanceID: core.StringPtr(instanceGuid),
+				Application: core.StringPtr("/opt/ibm/spark/examples/src/main/python/wordcount.py"),
+				//Class: core.StringPtr("testString"),
+				ApplicationArguments: []string{"/opt/ibm/spark/examples/src/main/resources/people.txt"},
+				//Conf: make(map[string]interface{}),
+				//Env: make(map[string]interface{}),
+				
+				
+			}
+			applicationResponse, response, err := ibmAnalyticsEngineApiService.CreateApplication(createApplicationOptions)
+			Expect(response.StatusCode).To(Equal(202))
+			Expect(applicationResponse).ToNot(BeNil())			
+			Expect(err).To(BeNil())
+			applicationId = *applicationResponse.ApplicationID
+			fmt.Printf("applicationResponse application_id : %v \n",applicationId)			
+		})
+	})
+
+	Describe(`GetApplications - Gets all applications submitted in an instance with a specified inst_id`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`GetApplications(getApplicationsOptions *GetApplicationsOptions)`, func() {
+
+			getApplicationsOptions := &ibmanalyticsengineapiv3.GetApplicationsOptions{
+				InstanceID: core.StringPtr(instanceGuid),
+			}
+
+			applicationCollection, response, err := ibmAnalyticsEngineApiService.GetApplications(getApplicationsOptions)
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(applicationCollection).ToNot(BeNil())
+
+		})
+	})
+
+	Describe(`GetApplicationByID - Gets the details of the application identified by the app_id identifier`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`GetApplicationByID(getApplicationByIdOptions *GetApplicationByIdOptions)`, func() {
+
+			getApplicationByIdOptions := &ibmanalyticsengineapiv3.GetApplicationByIdOptions{
+				InstanceID: core.StringPtr(instanceGuid),
+				ApplicationID: core.StringPtr(applicationId),
+			}
+
+			applicationGetResponse, response, err := ibmAnalyticsEngineApiService.GetApplicationByID(getApplicationByIdOptions)
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(applicationGetResponse).ToNot(BeNil())
+
+		})
+	})
+
+	Describe(`GetApplicationState - Gets the status of the application identified by the app_id identifier`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`GetApplicationState(getApplicationStateOptions *GetApplicationStateOptions)`, func() {
+
+			getApplicationStateOptions := &ibmanalyticsengineapiv3.GetApplicationStateOptions{
+				InstanceID: core.StringPtr(instanceGuid),
+				ApplicationID: core.StringPtr(applicationId),
+			}
+
+			applicationGetStateResponse, response, err := ibmAnalyticsEngineApiService.GetApplicationState(getApplicationStateOptions)
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(applicationGetStateResponse).ToNot(BeNil())
+
+		})
+	})
+
+	Describe(`DeleteApplicationByID - Stops the specified application`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`DeleteApplicationByID(deleteApplicationByIdOptions *DeleteApplicationByIdOptions)`, func() {
+
+			deleteApplicationByIdOptions := &ibmanalyticsengineapiv3.DeleteApplicationByIdOptions{
+				InstanceID: core.StringPtr(instanceGuid),
+				ApplicationID: core.StringPtr(applicationId),
+			}
+
+			response, err := ibmAnalyticsEngineApiService.DeleteApplicationByID(deleteApplicationByIdOptions)
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(204))
+
+		})
+	})
 })
+
+//
+// Utility functions are declared in the unit test file
+//
