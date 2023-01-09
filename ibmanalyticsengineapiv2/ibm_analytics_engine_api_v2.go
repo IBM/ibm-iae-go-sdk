bb8197110fa0706837a70ff766ea883fec2169e8
Subin Shekhar <subinpc@gmail.com>
2023-01-09 16:19:14 +0530

    fix: update module name for major version change
    Update module name to `github.com/IBM/ibm-iae-go-sdk/v2`.
    
    Signed-off-by: Subin Shekhar <subinpc@gmail.com>

--- ibmanalyticsengineapiv2/ibm_analytics_engine_api_v2.go
+++ ibmanalyticsengineapiv2/ibm_analytics_engine_api_v2.go
@@ -30,7 +30,7 @@ import (
 	"time"
 
 	"github.com/IBM/go-sdk-core/v5/core"
-	common "github.com/IBM/ibm-iae-go-sdk/common"
+	common "github.com/IBM/ibm-iae-go-sdk/v2/common"
 	"github.com/go-openapi/strfmt"
 )
 
